241828cf575416ef082f484f1cd368e74fc4af4c
your_username <hemanthchirikonda@gmail.com>
2020-09-23 02:32:47 +0530

    modified

--- gencode.js
+++ gencode.js
@@ -18,7 +18,7 @@ async function genaretecode(){
 
 
 }
-document.getElementById('usersubmit2').addEventListener("click",(e)=>{
+(document.getElementById('usersubmit2'))?.addEventListener("click",(e)=>{
 e.preventDefault();
 genaretecode();
 return false;
@@ -53,7 +53,7 @@ if(loginRes.token){
 }
 };
 
-document.getElementById('usersubmit1').addEventListener("click",(e)=>{
+(document.getElementById('usersubmit1'))?.addEventListener("click",(e)=>{
     e.preventDefault();
     verifycode();
     return false;
